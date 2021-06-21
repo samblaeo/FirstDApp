a9f84195d811c9480eb4eadd277b37f5ffcd31d2
Edu Samblas <edusamblas@Edus-MacBook-Pro.local>
2021-06-21 19:51:06 +0200

    .

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,56 @@ pragma solidity ^0.5.0;
 contract TodoList {
     
     uint public taskCount = 0;
+
+    struct Task{
+
+        uint id;
+
+        string content;
+
+        bool completed;
+    }
+
+    //Event to create a task from JS - Available inside the smart contract
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
+    //Event to complete a from JS - Available inside the smart contract
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+
+    constructor() public {
+
+        createTask("Check out Shieldpad.io");
+    }
+
+    mapping(uint => Task) public tasks;
+
+    //Function available from JS
+    function createTask(string memory _content) public {
+        
+        taskCount++;
+
+        tasks[taskCount] = Task(taskCount, _content, false);
+
+        //Lanzamos el evento
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+    //Function available from JS
+    function toggleCompleted(uint _id) public {
+
+        Task memory _task = tasks[_id];
+
+        _task.completed = !_task.completed;
+
+        tasks[_id] = _task;
+
+        emit TaskCompleted(_id, _task.completed);
+    }
 }
